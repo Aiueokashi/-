ca8e9263c47a177b82d664b3df50c1e2cfcfecf7
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-02-01 02:13:21 +0000

    Initial commit

---
+++ fetch.js
@@ -0,0 +1,32 @@
+const Promise = require('promise-polyfill');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+module.exports = (typeof fetch == 'function' ?
+    fetch.bind() :
+    (url, options) => {
+        return new Promise((res, rej) => {
+            const req = new XMLHttpRequest();
+            req.open(options.method || 'get', url);
+            req.withCredentials = options.credentials == 'include';
+
+            for (let k in options.headers || {}) {
+                req.setRequestHeader(k, options.headers[k]);
+            }
+
+            req.onload = () =>
+                res({
+                    status: req.status,
+                    statusText: req.statusText,
+                    text: () => Promise.resolve(req.responseText)
+                });
+            req.onerror = rej;
+
+            // Actually, fetch doesn't support onProgress feature
+            if (req.upload && options.onProgress) {
+                req.upload.onprogress = options.onProgress;
+            }
+
+            // Include body only if present
+            options.body ? req.send(options.body) : req.send();
+        });
+    });
---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const fetch = require("./fetch");
+
+var random = Math.floor(Math.random() * 10);
+temp = ['36.0', '36.1', '36.2', '36.3', '36.4', '36.5', '36.6', '36.7', '36.8', '36.9'];
+let url = `${process.env.FORM_URL}&entry.${process.env.ENTRY1}=${process.env.MY_GRADE}&entry.${process.env.ENTRY2}=1&entry.${process.env.ENTRY3}=29&entry.${process.env.ENTRY4}=${process.env.MY_NAME}&entry.${process.env.ENTRY5}=${temp[random]}&entry.${process.env.ENTRY6}=ない&entry.${process.env.ENTRY7}=ない&entry.${process.env.ENTRY8}=ない&entry.${process.env.ENTRY9}=いない&entry.${process.env.ENTRY10}=了承しました。`
+
+async function post_form() {
+  fetch(
+    encodeURI(url),
+    {
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  )
+    .then(() => console.log("posted"))
+    .catch((e) =>
+      console.log(e
+      )
+    );
+}
+
+post_form();
